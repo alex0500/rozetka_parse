1edf29a6605e7d481309df3fe9ec8c533cdf80dc
alex <jarne76@icloyd.com>
2019-02-02 22:49:33 +0200

    main python program

--- venv/parser.py
+++ venv/parser.py
@@ -9,7 +9,7 @@ import sys
 
 
 url = "https://rozetka.com.ua/mobile-phones/c80003/preset=smartfon/" # url from where parsing
-print(sys.argv[1])
+url = sys.argv[1]
 # checking if directory image exist or not
 if not os.access('./image', os.R_OK):
  os.mkdir('image')
